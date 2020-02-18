54a6a2145995977d00b412950747390a4203a197
Aryeh Leib Taurog <git@aryehleib.com>
2020-02-18 18:15:44 +0200

    Homework 2 - binary tree

--- src/LogAnalysis.hs
+++ src/LogAnalysis.hs
@@ -8,9 +8,9 @@ import Text.Read (readMaybe)
 
 import Log
 
+-- Exercise 1
 parse :: String -> [LogMessage]
 parse = lines >>> (map parseMessage)
-
 
 parseMessage :: String -> LogMessage
 parseMessage logMessage =
@@ -37,3 +37,48 @@ parseMessageType _ = Nothing
 parseTimestamp :: [String] -> Maybe (TimeStamp, [String])
 parseTimestamp (x:xs) = (readMaybe x) <&> (,xs)
 parseTimestamp _ = Nothing
+
+-- Exercise 2
+insert :: LogMessage -> MessageTree -> MessageTree
+insert a tree@(Node left b right) =
+  case compareLog a b of
+    Just GT -> Node left b (insert a right)
+    Nothing -> tree
+    _ -> Node (insert a left) b right
+insert (Unknown _) Leaf = Leaf
+insert a Leaf = Node Leaf a Leaf
+
+
+compareLog :: LogMessage -> LogMessage -> Maybe Ordering
+compareLog (LogMessage _ a _) (LogMessage _ b _) = Just $ compare a b
+compareLog _ _ = Nothing
+
+
+-- Exercise 3
+build :: [LogMessage] -> MessageTree
+build = foldl (flip insert) Leaf
+
+
+-- Exercise 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left m right) = (inOrder left) ++ (m : (inOrder right))
+
+
+-- Exercise 5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong =
+  filter severe
+  >>> build
+  >>> inOrder
+  >>> map messageBody
+
+
+severe :: LogMessage -> Bool
+severe (LogMessage (Error level) _ _) = level >= 50
+severe _ = False
+
+
+messageBody :: LogMessage -> String
+messageBody (LogMessage _ _ body) = body
+messageBody (Unknown body) = body
--- test/TestHomework02.hs
+++ test/TestHomework02.hs
@@ -10,6 +10,10 @@ import LogAnalysis
 tests :: TestTree
 tests = testGroup "Homework 02"
     [ testEx01
+    , testEx02
+    , testEx03
+    , testEx04
+    , testEx05
     ]
 
 testEx01 :: TestTree
@@ -44,3 +48,92 @@ testEx01 = testGroup "Exercise 1 - parse a single message"
         , ("This is not in the right format", Unknown "This is not in the right format")
         ]
     ]
+
+msgA :: LogMessage
+msgA = LogMessage Info 5 "alfalfa"
+
+msgB :: LogMessage
+msgB = LogMessage Warning 8 "cottage"
+
+msgC :: LogMessage
+msgC = LogMessage (Error 3) 9 "kipling"
+
+msgD :: LogMessage
+msgD = Unknown "depths"
+
+tree :: MessageTree
+tree = Node Leaf msgB Leaf
+
+testEx02 :: TestTree
+testEx02 = testGroup "Exercise 2 - binary tree insert"
+    [ param1 "insert tree" (flip insert tree)
+        [ (msgA, Node (Node Leaf msgA Leaf) msgB Leaf)
+        , (msgB, Node (Node Leaf msgB Leaf) msgB Leaf)
+        , (msgC, Node Leaf msgB (Node Leaf msgC Leaf))
+        , (Unknown "foobar", tree)
+        ]
+    , param1 "insert leaf" (flip insert Leaf)
+        [ (msgA, Node Leaf msgA Leaf)
+        , (Unknown "loop", Leaf)
+        ]
+    , param1 "compareLog" (compareLog msgB)
+        [ (msgA, Just GT)
+        , (msgB, Just EQ)
+        , (msgC, Just LT)
+        , (Unknown "nada", Nothing)
+        ]
+    ]
+
+
+testEx03 :: TestTree
+testEx03 = testGroup "Exercise 3 - tree building"
+    [ param1 "build" build
+        [ ([msgB, msgA, msgC, msgD],
+            Node (Node Leaf msgA Leaf) msgB (Node Leaf msgC Leaf))
+        ]
+    ]
+
+
+testEx04 :: TestTree
+testEx04 = testGroup "Exercise 4 - tree traversal"
+    [ param1 "inOrder" (inOrder . build)
+        [ ([msgB, msgA, msgC, msgD], [msgA, msgB, msgC])
+        ]
+    ]
+
+
+testEx05 :: TestTree
+testEx05 = testGroup "Exercise 5 - log ordering and filtering"
+    [ param1 "whatWentWrong" (whatWentWrong . parse)
+        [ (input, expected) ]
+    , param1 "severe" severe
+        [ (msgA, False)
+        , (msgB, False)
+        , (msgC, False)
+        , (msgD, False)
+        , (LogMessage (Error 50) 2 "hi", True)
+        ]
+    , param1 "messageBody" messageBody
+        [ (LogMessage (Error 50) 2 "hi", "hi")
+        , (LogMessage Info 2 "I love Haskell", "I love Haskell")
+        ]
+    ]
+    where
+        input = unlines
+            [ "I 6 Completed armadillo processing"
+            , "I 1 Nothing to report"
+            , "E 99 10 Flange failed!"
+            , "I 4 Everything normal"
+            , "I 11 Initiating self-destruct sequence"
+            , "E 70 3 Way too many pickles"
+            , "E 65 8 Bad pickle-flange interaction detected"
+            , "W 5 Flange is due for a check-up"
+            , "I 7 Out for lunch, back in two time steps"
+            , "E 20 2 Too many pickles"
+            , "I 9 Back from lunch"
+            ]
+        expected =
+            [ "Way too many pickles"
+            , "Bad pickle-flange interaction detected"
+            , "Flange failed!"
+            ]
