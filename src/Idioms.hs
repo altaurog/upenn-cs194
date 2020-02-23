98acd21fc32311628561e1b3fd602500d182478b
Aryeh Leib Taurog <git@aryehleib.com>
2020-02-23 21:32:57 +0200

    Homework 4 - AVL Tree

--- src/Idioms.hs
+++ src/Idioms.hs
@@ -2,6 +2,7 @@ module Idioms where
 
 import Control.Arrow ((>>>))
 
+-- Exercise 01
 fun1' :: [Integer] -> Integer
 fun1' = foldl f 1
     where f agg v
@@ -27,9 +28,96 @@ fun1 (x:xs)
     | otherwise = fun1 xs
 
 
--- apparently this is what was intended
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n
     | even n    = n + fun2 (n `div` 2)
     | otherwise = fun2 (3 * n + 1)
+
+
+-- Exercise 02
+-- AVL Tree
+data Tree a = Leaf
+        | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+-- “constructor”
+
+node :: a -> Tree a -> Tree a -> Tree a
+node v a b =
+    let h = 1 + max (height a) (height b)
+    in Node h a v b
+
+
+foldTree :: Ord a => [a] -> Tree a
+foldTree = foldr (flip insert) Leaf
+
+-- “accessor” functions
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
+
+{-|
+ - rotate right:
+ -
+ -      a             b
+ -     / \           / \
+ -    b   t3   =>   t1  a
+ -   / \               / \
+ -  t1  t2            t2  t3
+ -}
+rotateRight :: Tree a -> Tree a
+rotateRight (Node _ (Node _ t1 bv t2) av t3) =
+    let ah' = 1 + max (height t2) (height t3)
+        bh' = 1 + max (height t1) ah'
+    in Node bh' t1 bv (Node ah' t2 av t3)
+rotateRight a = a
+
+
+{-|
+ - rotate left:
+ -
+ -     a                b
+ -    / \              / \
+ -   t1  b      =>    a   t3
+ -      / \          / \
+ -     t2  t3       t1  t2
+ -}
+rotateLeft :: Tree a -> Tree a
+rotateLeft (Node _ t1 av (Node _ t2 bv t3)) =
+    let ah' = 1 + max (height t1) (height t2)
+        bh' = 1 + max (height t3) ah'
+    in Node bh' (Node ah' t1 av t2) bv t3
+rotateLeft a = a
+
+
+insert :: Ord a => Tree a -> a -> Tree a
+insert Leaf x = Node 0 Leaf x Leaf
+insert (Node _ left v right) x
+    | x < v     = _insert v (insert left x) right
+    | otherwise = _insert v left (insert right x)
+
+
+-- rebalance sides if necessary
+_insert :: Ord a => a -> (Tree a) -> (Tree a) -> Tree a
+_insert v a b = case balance a b of
+    EQ -> node v a b
+    LT -> rotateLeft $ node v a (check LT b)
+    GT -> rotateRight $ node v (check GT a) b
+
+
+-- check child node when rotating
+check :: Ord a => Ordering -> Tree a -> Tree a
+check o t@(Node _ a _ b)
+    | o == LT && (height a) > (height b) = rotateRight t
+    | o == GT && (height a) < (height b) = rotateLeft t
+    | otherwise         = t
+check _ t = t
+
+
+balance :: Tree a -> Tree a -> Ordering
+balance a b 
+    | height a - height b < -1  = LT
+    | height a - height b > 1   = GT
+    | otherwise                 = EQ
--- test/TestHomework04.hs
+++ test/TestHomework04.hs
@@ -9,6 +9,7 @@ import Idioms
 tests :: TestTree
 tests = testGroup "Homework 03"
     [ testEx01
+    , testEx02
     ]
 
 testEx01 :: TestTree
@@ -25,3 +26,37 @@ testEx01 = testGroup "Exercise 1 - Wholemeal programming"
     , matchOutput "fun2" fun2' fun2 $
         [ 1 .. 10 ] ++ [15, 18, 25]
     ]
+
+
+
+testEx02 :: TestTree
+testEx02 = testGroup "Exercise 2 - AVL Tree"
+    [ param1 "rotateRight" rotateRight
+        [ (Leaf, Leaf)
+        , (a, a)
+        , ((Node 2 (Node 1 a 15 b) 25 c), (Node 2 a 15 (Node 1 b 25 c)))
+        ]
+    , param1 "rotateLeft" rotateLeft
+        [ (Leaf, Leaf)
+        , (a, a)
+        , ((Node 2 a 15 (Node 1 b 25 c)), (Node 2 (Node 1 a 15 b) 25 c))
+        ]
+    , param1 "foldTree" foldTree
+        [ ([], Leaf :: Tree Integer)
+        , ([10], a)
+        , ([10, 20], Node 1 a 20 Leaf)
+        , ([10, 20, 30], Node 1 a 20 c)
+        , ([20, 10, 30], Node 1 a 20 c)
+        , ([30, 20, 10], Node 1 a 20 c)
+        , ([30, 10, 20], Node 1 a 20 c)
+        , ([10, 20, 30, 40], Node 2 (Node 1 a 20 Leaf) 30 d)
+        , ([10, 20, 30, 40, 50], Node 2 (Node 1 a 20 c) 40 e)
+        , ([20, 10, 30, 40, 50], Node 2 (Node 1 a 20 c) 40 e)
+        ]
+    ]
+    where
+        a = Node 0 Leaf 10 Leaf :: Tree Integer
+        b = Node 0 Leaf 20 Leaf
+        c = Node 0 Leaf 30 Leaf
+        d = Node 0 Leaf 40 Leaf
+        e = Node 0 Leaf 50 Leaf
