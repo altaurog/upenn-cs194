f59a2d5e6685639c03ba7b17444ef4882d06dc3e
Aryeh Leib Taurog <git@aryehleib.com>
2020-04-16 18:29:58 +0300

    Homework 6 - better Fibonacci

--- src/Fibonacci.hs
+++ src/Fibonacci.hs
@@ -1,6 +1,7 @@
 module Fibonacci where
 
 import Data.Function.Memoize
+import qualified Data.Map as M
 
 -- Homework 6
 -- exercise 1
@@ -12,7 +13,7 @@ fib n = (fib (n - 1)) + (fib (n - 2))
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
--- exercise 2
+-- exercise 2 - dumb approach
 fib2 = memoize fibm
 
 fibm :: Integer -> Integer
@@ -22,3 +23,14 @@ fibm n = (fib2 (n - 1)) + (fib2 (n - 2))
 
 fibs2 :: [Integer]
 fibs2 = map fib2 [0..]
+
+-- exercise 2 - handcrafted approach
+{- fibonacci sequence is inherently recursive,
+ - don’t need to compute as function of index,
+ - but do need additional state (penultimate value)
+ -}
+fibs2' :: [Integer]
+fibs2' = map fst $ fm (0, 1)
+
+fm :: (Integer, Integer) -> [(Integer, Integer)]
+fm (a, b) = (a, b) : (fm (b, a + b))
--- test/TestHomework06.hs
+++ test/TestHomework06.hs
@@ -11,6 +11,7 @@ tests :: TestTree
 tests = testGroup "Homework 06"
     [ testEx01
     , testEx02
+    , testEx02a
     ]
 
 testEx01 :: TestTree
@@ -38,10 +39,32 @@ testEx01 = testGroup "Exercise 1 - Fibonacci"
     ]
 
 testEx02 :: TestTree
-testEx02 = localOption (mkTimeout 2) $ testGroup "Exercise 2 - Faster Fibonacci"
+testEx02 = localOption (mkTimeout 2) $ testGroup "Exercise 2 - Faster Fibonacci I"
     [ param1 "fib2" fib2 [(16, 987)]
-    , testCase "fibs1" $
+    , testCase "fibs2" $
         take 15 fibs2 @?=
+            [ 0
+            , 1
+            , 1
+            , 2
+            , 3
+            , 5
+            , 8
+            , 13
+            , 21
+            , 34
+            , 55
+            , 89
+            , 144
+            , 233
+            , 377
+            ]
+    ]
+
+testEx02a :: TestTree
+testEx02a = localOption (mkTimeout 1) $ testGroup "Exercise 2 - Faster Fibonacci II"
+    [ testCase "fibs2'" $
+        take 15 fibs2' @?=
             [ 0
             , 1
             , 1
